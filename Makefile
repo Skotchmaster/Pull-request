2e2b8dac4085cc5d079171ae19e83a698e2bc738
Skotchmaster <sava_nedno@mail.ru>
2025-11-16 17:10:20 +0300

    docker-compose update(create test containers) and small env update

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 BINARY_NAME = pr-service
 
-.PHONY: build run test tidy up down logs
+.PHONY: build run test tidy up down logs loadtest
 
 build:
 	go build -o bin/$(BINARY_NAME) ./cmd/server
@@ -18,10 +18,22 @@ lint:
 	golangci-lint run ./...
 
 up:
-	docker-compose up --build
+	docker-compose --profile dev up -d --build
+
+up-test:
+	docker-compose --profile test up -d --build
 
 down:
-	docker-compose down -v
+	docker-compose --profile dev down -v
+
+down-test:
+	docker-compose --profile test down -v
 
 logs:
-	docker-compose logs -f
+	docker-compose --profile dev logs -f
+
+logs-test:
+	docker-compose --profile test logs -f
+
+loadtest:
+	k6 run loadtest/pr_loadtest.js
--- internal/config/config.go
+++ internal/config/config.go
@@ -3,13 +3,17 @@ package config
 import (
 	"fmt"
 	"os"
-	"pull_request/internal/logging"
 
+	"pull_request/internal/logging"
 )
 
 type Config struct {
-	DBURL     string
-	Port      string
+	DBUser string
+	DBPass string
+	DBHost string
+	DBPort string
+	DBName string
+	Port   string
 }
 
 func mustEnv(name string) (string, error) {
@@ -21,19 +25,56 @@ func mustEnv(name string) (string, error) {
 }
 
 func Load(logger logging.Logger) (*Config, error) {
-	dsn, err := mustEnv("DB_URL")
+	user, err := mustEnv("DB_USER")
 	if err != nil {
 		return nil, err
 	}
 
-	port := os.Getenv("PORT")
+	pass, err := mustEnv("DB_PASSWORD")
+	if err != nil {
+		return nil, err
+	}
+
+	name, err := mustEnv("DB_NAME")
+	if err != nil {
+		return nil, err
+	}
+
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = "localhost"
+		logger.Infof("DB_HOST not set, using default %s", host)
+	}
+
+	port := os.Getenv("DB_PORT")
 	if port == "" {
-		port = "8080"
-		logger.Infof("PORT not set, using default %s", port)
+		port = "5432"
+		logger.Infof("DB_PORT not set, using default %s", port)
+	}
+
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+		logger.Infof("PORT not set, using default %s", httpPort)
 	}
 
 	return &Config{
-		DBURL:     dsn,
-		Port:      port,
+		DBUser: user,
+		DBPass: pass,
+		DBHost: host,
+		DBPort: port,
+		DBName: name,
+		Port:   httpPort,
 	}, nil
+}
+
+func (c *Config) DBURL() string {
+	return fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		c.DBUser,
+		c.DBPass,
+		c.DBHost,
+		c.DBPort,
+		c.DBName,
+	)
 }
